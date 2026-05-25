043084d2b1afdedabc917c8d34dcd5c7afc0d4dc
Chris Donnan <cdonnan60@gmail.com>
2026-05-25 11:12:35 +1000

    1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     public int maxPickups = 5;
     public bool levelComplete = false;
     public Text pickupText;
-    // Audio Proxiiy Logic
+    // Audio Proximity Logic
     public AudioSource[] audioSources;
     public float audioProximity = 5.0f;
     public void UpdateGUI()
